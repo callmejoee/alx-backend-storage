bc8ad1b364a3a621f6c3c6b574335bab3a46ab12
Youssef <youssefkaramedu@gmail.com>
2024-07-08 15:43:09 +0000

    Add func 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float, None]:
+        """Retrieve the data from Redis using the
+        key and convert it using the given function if provided."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve the data as a string."""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve the data as an integer."""
+        return self.get(key, fn=int)
